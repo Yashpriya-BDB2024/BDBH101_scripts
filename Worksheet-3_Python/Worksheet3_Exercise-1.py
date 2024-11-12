9efed3c193636d41ea247cf2613911a39e717ab1
Yash Priya Baid <yashpriyabaid@gmail.com>
2024-11-12 11:26:18 +0530

    Update Worksheet3_Exercise-1.py

--- Worksheet-3_Python/Worksheet3_Exercise-1.py
+++ Worksheet-3_Python/Worksheet3_Exercise-1.py
@@ -2,7 +2,8 @@
 ### Create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...].
 
 def capitalize(fruits):
-    capitalized_fruits=[fruits.capitalize() for fruits in fruits]
+# Each fruit present in the input list will get capitalized & will be stored in the list called 'capitalized_fruits'.
+    capitalized_fruits=[fruits.capitalize() for fruits in fruits]   
     print(f"Capitalized fruits: {capitalized_fruits}")
 def main():
     fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']

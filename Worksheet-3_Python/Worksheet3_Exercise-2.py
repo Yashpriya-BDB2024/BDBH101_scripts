19d3fb994cb431f378659ddf7af26520b6e75638
Yash Priya Baid <yashpriyabaid@gmail.com>
2024-11-12 17:15:07 +0530

    Update Worksheet3_Exercise-2.py

--- Worksheet-3_Python/Worksheet3_Exercise-2.py
+++ Worksheet-3_Python/Worksheet3_Exercise-2.py
@@ -4,6 +4,8 @@
 
 def fruits_with_vowels(fruits):
     vowels = 'aeiou'
+# For each fruit, count the total occurrences of vowels.
+# If the count is 2, then, include it in the resulting list.
     fruits_with_only_two_vowels = [fruit for fruit in fruits if sum(fruit.count(char) for char in vowels) == 2]
     print(f"Fruits with only 2 vowels: {fruits_with_only_two_vowels}")
 def main():
